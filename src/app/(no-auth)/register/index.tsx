49f90a17d85d56b8075635fa2e22234332806889
maragnaaa <gustavomaragna88@gmail.com>
2024-12-04 01:26:04 -0300

    Update Register

--- src/app/(no-auth)/register/index.tsx
+++ src/app/(no-auth)/register/index.tsx
@@ -12,40 +12,40 @@ export default function Register() {
       </View>
 
       <View>
-        <Text className="text-[66px] font-black text-center pt-[7rem]">
-          Bem-vindo!
+        <Text className="text-[66px] font-bold text-center pt-[7rem] font-inter-regular">
+          Bem-Vindo!
         </Text>
-        <Text className="text-[20px] font-bold text-center pr-[65px] pl-[65px]">
+        <Text className="text-[20px] font-bold text-center pr-[65px] pl-[65px] font-inter-regular">
           Lorem Ipsum is simply dummy text of the 
           printing and typesetting industry. 
         </Text>
       </View>
 
       <View className="items-center pt-[50px] gap-3">
-      <Input placeholder="Nome"/>
-      <Input placeholder="Email*"/>
-      <Input placeholder="Senha*"/>
-      <Input placeholder="Confirmar Senha*"/>
+      <Input placeholder="Nome" className="placeholder:text-gray-500 font-inter-regular"/>
+      <Input placeholder="Email*" className="placeholder:text-gray-500 font-inter-regular"/>
+      <Input placeholder="Senha*" className="placeholder:text-gray-500 font-inter-regular"/>
+      <Input placeholder="Confirmar Senha*" className="placeholder:text-gray-500 font-inter-regular"/>
       </View>
 
       <View className="pt-[30px] items-center">
-      <Button text="Criar Conta"/>
+      <Button text="Criar Conta" className="font-inter-regular"/>
       </View>
 
       <View className="gap-5 pt-10">
-        <Text className="pl-[120px] w-[330]">
+        <Text className="pl-[120px] w-[330] font-inter-regular">
           Li e estou de acordo com o Termo de Uso e Política de Privacidade
         </Text>
-        <Text className="pl-[120px] w-[330]">
+        <Text className="pl-[120px] w-[330] font-inter-regular">
           Aceito receber Emails e SMS de novidades do Fisioterapia
         </Text>
       </View>
 
-      <View className="flex flex-row pl-[25px] pt-[130px] gap-[100px] m-[0px]">
-        <Text className="text-[#1F4BFC]" style={{opacity: 0.3}}>
+      <View className="flex flex-row pl-[18px] pt-[130px] gap-[100px] m-[0px]">
+        <Text className="text-[#1F4BFC] font-inter-regular" style={{opacity: 0.3}}>
           Dúvidas? Fale Conosco
         </Text>
-        <Text className="text-[#1F4BFC]" style={{opacity: 0.3}}>
+        <Text className="text-[#1F4BFC] font-inter-regular" style={{opacity: 0.3}}>
           Todos os direitos reservados
         </Text>
       </View>
