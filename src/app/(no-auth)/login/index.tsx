309d0bfa4f6bf81cae917084dae9bff4edb2f74b
Miguel Almeida <miguelh.almeida05@gmail.com>
2024-12-05 00:09:51 -0300

    Alteração home
    implementação do menu

--- src/app/(no-auth)/login/index.tsx
+++ src/app/(no-auth)/login/index.tsx
@@ -10,37 +10,30 @@ export default function Login() {
         <TopButton text="Entrar" href="/(no-auth)/login" selected={true} />
         <TopButton text="Registrar-se" href="/(no-auth)/register" />
       </View>
-      
-    <View className="flex-1 items-center justify-center gap-3">
 
-    <View>
-      <Text className="text-[1.5rem] text-center font-bold mb-6">
-        Bem-vindo ao FisioSync
-      </Text>
-    </View>
+      <View className="flex-1 items-center justify-center gap-3">
+        <View>
+          <Text className="text-[1.5rem] text-center font-bold mb-6">Bem-vindo ao FisioSync</Text>
+        </View>
 
-    <Input placeholder="Email ou número de telefone" className="placeholder:text-gray-500 font-inter-regular"/>
+        <Input placeholder="Email ou número de telefone" className="placeholder:text-gray-500 font-inter-regular" />
 
-<Input placeholder="Senha" className="placeholder:text-gray-500 font-inter-regular"/>
+        <Input placeholder="Senha" className="placeholder:text-gray-500 font-inter-regular" />
 
-<Button text="Log in"/>
+        <Button text="Log in" />
 
-      <Text className="font-semibold text-center text-gray-500 text-lg m-1">
-        OU
-      </Text>
+        <Text className="font-semibold text-center text-gray-500 text-lg m-1">OU</Text>
 
-      <Button text="Continuar com Facebook" className="bg-[#385CC6]"/>
+        <Button text="Continuar com Facebook" className="bg-[#385CC6]" />
 
-      <Button text="Continuar com Google" className="bg-[#507CFF]"/>
+        <Button text="Continuar com Google" className="bg-[#507CFF]" />
 
         <Link href={"/(no-auth)/forget"} className="mt-7 text-gray-500">
           Esqueceu a senha?
         </Link>
 
-        <View className="w-56 border-t border-gray-400"/>
-
-    </View>
-
+        <View className="w-56 border-t border-gray-400" />
+      </View>
     </Page>
   );
 }
