6c599e728138549bccf43a472813acdd7e7e6d5f
Gabriel Silva <62155272+bielsiilva@users.noreply.github.com>
2024-12-06 20:43:34 -0300

    Update index.tsx

--- src/app/(with-auth)/(tabs-main)/search/index.tsx
+++ src/app/(with-auth)/(tabs-main)/search/index.tsx
@@ -4,70 +4,69 @@ import { Page , Input } from "@/components/template";
 export default function Search() {
   return (
     <Page>
-      <View className="flex-1 px-8 sm:px-10 py-10 flex flex-col">
+      <View className="flex-1 px-4 sm:px-8 py-10">
 
-        <View className="pt-10 flex items-center gap-6">
+        <View className="pt-16 w-full flex items-center gap-5">
+          <Text className="text-center text-xl font-bold">Escolha sua música favorita</Text>
+          <Text className="text-center text-sm font-light pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat turpis vitae enim pretium elementum.</Text>
 
-          <Text className="text-center text-xl font-medium">Escolha sua música favorita</Text>
-          <Text className="text-center text-sm font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat turpis vitae enim pretium elementum. Vestibulum laoreet magna, eu porttitor augue sollicitudin eu.</Text>
-
-          <Input placeholder="Pesquise sua música aqui..." className="w-full h-auto bg-gray-100 border border-gray-100 rounded-2xl px-2 py-1 text-base font-light"/>
+          <Input placeholder="Pesquise sua música aqui..." className="w-full max-w-xl h-[40px] bg-gray-100 border-0 rounded-2xl px-4 text-base font-light"/>
         </View>
           
-        <View className="h-auto flex flex-col space-y-6 gap-4 w-full p-4">
+        <View className="h-auto flex flex-col space-y-6 pt-10 gap-1 w-full">
 
-          <Text className="text-left text-2xl text-xl font-medium">Músicas BPM</Text>
+          <Text className="text-left text-xl font-bold">Músicas BPM</Text>
 
-          <View className="h-auto flex flex-wrap justify-between gap-4 w-full p-4">
+          <View className="h-auto flex flex-wrap justify-between gap-4 w-full pt-4 pb-2">
             <View className="flex flex-row justify-between gap-4 w-full">
-              <View className="flex flex-col items-center flex-1">
-                <View className="bg-gray-200 p-4 rounded-xl w-full h-[100px]"/>
-                <Text className="text-center text-lg font-medium">Álbum 1</Text>
-                <Text className="text-center text-sm font-light">Artista</Text>
+              <View className="flex flex-col items-stretch flex-1">
+                <View className="bg-gray-200 rounded-xl w-full h-[100px]"/>
+                <Text className="pt-2 text-left text-lg font-medium">Álbum 1</Text>
+                <Text className="text-left text-sm font-light">Artista</Text>
               </View>
 
-              <View className="flex flex-col items-center flex-1">
-                <View className="bg-gray-200 p-4 rounded-xl w-full h-[100px]"/>
-                <Text className="text-center text-lg font-medium">Álbum 2</Text>
-                <Text className="text-center text-sm font-light">Artista</Text>
+              <View className="flex flex-col items-stretch flex-1">
+                <View className="bg-gray-200 rounded-xl w-full h-[100px]"/>
+                <Text className="pt-2 text-left text-lg font-medium">Álbum 2</Text>
+                <Text className="text-left text-sm font-light">Artista</Text>
               </View>
 
-              <View className="flex flex-col items-center flex-1">
-                <View className="bg-gray-200 p-4 rounded-xl w-full h-[100px]"/>
-                <Text className="text-center text-lg font-medium">Álbum 3</Text>
-                <Text className="text-center text-sm font-light">Artista</Text>
+              <View className="flex flex-col items-stretch flex-1">
+                <View className="bg-gray-200 rounded-xl w-full h-[100px]"/>
+                <Text className="pt-2 text-left text-lg font-medium">Álbum 3</Text>
+                <Text className="text-left text-sm font-light">Artista</Text>
               </View>
             </View>
           </View>
         </View>
 
-        <View className="h-auto flex flex-wrap space-y-6 gap-4 w-full">
+        <View className="h-auto flex flex-wrap space-y-6 pt-6 gap-1 w-full">
 
-          <Text className="text-left text-2xl text-xl font-medium mb-4">Navegar por gêneros</Text>
+          <Text className="text-left text-xl font-bold">Navegar por gêneros</Text>
 
-          <View className="flex flex-wrap justify-between gap-4">
-            <View className="flex flex-col items-center flex-4">
-              <View className="w-[48%] bg-gray-200 rounded-lg p-4 h-[120px]"/>
+          <View className="flex-row flex-wrap justify-around gap-4 pt-4">
+            <View className="flex flex-col items-center w-[30%]">
+              <View className="bg-gray-200 rounded-lg w-full h-[65px]"/>
             </View>
 
             <View className="flex flex-col items-center w-[30%]">
-              <View className="w-[48%] bg-gray-200 rounded-lg p-4 h-[120px]"/>
+              <View className="bg-gray-200 rounded-lg w-full h-[65px]"/>
             </View>
 
             <View className="flex flex-col items-center w-[30%]">
-              <View className="w-[48%] bg-gray-200 rounded-lg p-4 h-[120px]"/>
+              <View className="bg-gray-200 rounded-lg w-full h-[65px]"/>
             </View>
 
             <View className="flex flex-col items-center w-[30%]">
-              <View className="w-[48%] bg-gray-200 rounded-lg p-4 h-[120px]"/>
+              <View className="bg-gray-200 rounded-lg w-full h-[65px]"/>
             </View>
 
             <View className="flex flex-col items-center w-[30%]">
-              <View className="w-[48%] bg-gray-200 rounded-lg p-4 h-[120px]"/>
+              <View className="bg-gray-200 rounded-lg w-full h-[65px]"/>
             </View>
 
             <View className="flex flex-col items-center w-[30%]">
-              <View className="=w-[48%] bg-gray-200 rounded-lg p-4 h-[120px]"/>
+              <View className="bg-gray-200 rounded-lg w-full h-[65px]"/>
             </View>
           </View>
         </View>
