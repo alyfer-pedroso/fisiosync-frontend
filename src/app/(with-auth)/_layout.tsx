6b98bc752c53cbc28bdb131d3f61a07e729dd7d7
Alyfer Pedroso <138719366+alyfer-pedroso@users.noreply.github.com>
2024-12-05 23:04:53 -0300

    Update _layout.tsx

--- src/app/(with-auth)/_layout.tsx
+++ src/app/(with-auth)/_layout.tsx
@@ -10,7 +10,8 @@ export default function Layout() {
   useEffect(() => {
     (async () => {
       handleLoading(true);
-      !(await getData("token")) && router.push("/(no-auth)/login");
+      // Descomentar padrão depois
+      //* Padrão: !(await getData("token")) && router.push("/(no-auth)/login");
       handleLoading(false);
     })();
   }, []);
