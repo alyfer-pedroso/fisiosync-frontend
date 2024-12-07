32e37c65a9d3703016e830a4ccf764e957fc4b6f
Miguel Almeida <miguelh.almeida05@gmail.com>
2024-12-06 23:54:12 -0300

    Install slider, tudo ok

--- src/app/(with-auth)/home/index.tsx
+++ src/app/(with-auth)/home/index.tsx
@@ -1,8 +1,28 @@
 import { Text } from "react-native";
 import { Page } from "@/components/template";
 import { View, TouchableOpacity, ScrollView } from "react-native";
+import Slider from "@react-native-community/slider";
+import { useState, useCallback } from "react";
+import { debounce } from "lodash";
 
 export default function Home() {
+  const [sliderValue, setSliderValue] = useState(100);
+
+  const handleSliderChange = useCallback(
+    debounce((value) => {
+      setSliderValue(value);
+    }, 100),
+    []
+  );
+
+  const handleDecrease = () => {
+    setSliderValue((prevValue) => Math.max(prevValue - 1, 0));
+  };
+
+  const handleIncrease = () => {
+    setSliderValue((prevValue) => Math.min(prevValue + 1, 200));
+  };
+
   return (
     <Page>
       <View className="w-full flex-1 px-8 pb-8">
@@ -13,25 +33,67 @@ export default function Home() {
         </View>
 
         <View className="flex-1 items-center gap-4">
-          <View className="bg-[#EDEDED] w-full h-52 rounded-lg items-center mb-4">
-            <View className="flex-1 pl-6 justify-center">
-              <View className="gap-4 flex-row ">
-                <View className="bg-[#1F4BFC] w-8 h-8 rounded-full opacity-50 items-center justify-center"><Text className="text-[#0D006F] font-bold text-center">1</Text></View>
-                <View className="bg-[#A1A1A1] w-8 h-8 rounded-full opacity-50 items-center justify-center"><Text className="text-[#FFFFFF] font-bold text-center">2</Text></View>
-                <View className="bg-[#A1A1A1] w-8 h-8 rounded-full opacity-50 items-center justify-center"><Text className="text-[#FFFFFF] font-bold text-center">3</Text></View>
-                <View className="bg-[#A1A1A1] w-8 h-8 rounded-full opacity-50 items-center justify-center"><Text className="text-[#FFFFFF] font-bold text-center">4</Text></View>
+          <View className="bg-[#EDEDED] w-full h-56 rounded-lg items-center justify-center">
+            <View className="flex-row gap-4 items-center justify-center mb-4">
+              <View className="bg-[#1F4BFC] w-8 h-8 rounded-full opacity-50 items-center justify-center">
+                <Text className="text-[#0D006F] font-bold text-center w-full">1</Text>
               </View>
-              
+              <View className="bg-[#A1A1A1] w-8 h-8 rounded-full opacity-50 items-center justify-center">
+                <Text className="text-[#FFFFFF] font-bold">2</Text>
+              </View>
+              <View className="bg-[#A1A1A1] w-8 h-8 rounded-full opacity-50 items-center justify-center">
+                <Text className="text-[#FFFFFF] font-bold">3</Text>
+              </View>
+              <View className="bg-[#A1A1A1] w-8 h-8 rounded-full opacity-50 items-center justify-center">
+                <Text className="text-[#FFFFFF] font-bold">4</Text>
+              </View>
             </View>
-            <TouchableOpacity className="rounded-md bg-[#1F4BFC] w-28 h-10 justify-center m-5">
-              <Text className="text-center text-[#ffff] font-bold ">INICIAR</Text>
+
+            <View className="w-3/4 mb-2 flex-row items-center justify-center gap-4">
+              <TouchableOpacity
+                className="bg-[#1F4BFC] p-2 rounded-full"
+                onPress={handleDecrease}
+                style={{ width: 30, height: 30 }}
+              >
+                <Text className="text-white font-bold text-center">-</Text>
+              </TouchableOpacity>
+
+              <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <View style={{ height: 10, width: 150, backgroundColor: "#B7B7B7", borderRadius: 5 }}>
+                  <Slider
+                    style={{ height: 10 }}
+                    minimumValue={0}
+                    maximumValue={200}
+                    step={1}
+                    value={sliderValue}
+                    onValueChange={handleSliderChange}
+                    minimumTrackTintColor="#B7B7B7"
+                    maximumTrackTintColor="#B7B7B7"
+                    thumbTintColor="#1F4BFC"
+                  />
+                </View>
+              </View>
+
+              <TouchableOpacity
+                className="bg-[#1F4BFC] p-2 rounded-full"
+                onPress={handleIncrease}
+                style={{ width: 30, height: 30 }}
+              >
+                <Text className="text-white font-bold text-center">+</Text>
+              </TouchableOpacity>
+            </View>
+
+            <Text className="font-bold text-2xl mt-2 pb-2">{sliderValue} BPM</Text>
+
+            <TouchableOpacity className="rounded-md bg-[#1F4BFC] w-36 h-12 items-center justify-center">
+              <Text className="text-center text-[#FFFF] font-bold">INICIAR</Text>
             </TouchableOpacity>
           </View>
         </View>
 
         <View className="mt-4 mb-3">
           <View className="flex-row items-center justify-between">
-            <Text className="font-extrabold text-lg leading-tight">Musicas e frequencias</Text>
+            <Text className="font-extrabold text-lg leading-tight">Músicas e Frequencias</Text>
             <Text className="font-medium text-[#1F4BFC] max-w-[30%] truncate text-right text-xs">Veja mais</Text>
           </View>
         </View>
