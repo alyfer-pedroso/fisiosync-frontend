43b3e547a95fa2c0a40ada1d1c8182faa6a2eff7
maragnaaa <gustavomaragna88@gmail.com>
2024-12-07 12:08:17 -0300

    Config user

--- src/app/(with-auth)/user/components/configuser/index.tsx
+++ src/app/(with-auth)/user/components/configuser/index.tsx
@@ -10,7 +10,7 @@ export function ConfigUser({...props}: Models.IUserHook) {
     <Page className="w-full flex-1">
       <View className="flex-row items-center space-x-4">
         <View className="bg-[#EDEDED] h-36 w-36 rounded-full ml-5"></View>
-        <View>
+          <View>
           <Text className="text-[50px] font-inter-bold w-60 items-center ml-3 text-wrap">Miguel Almeida</Text>
         </View>
       </View>
@@ -26,6 +26,11 @@ export function ConfigUser({...props}: Models.IUserHook) {
             <Text className="text-[20px] font-black font-inter-Black">Telefone</Text>
             <Input className="w-[190px] text-gray-500 font-inter-regular" placeholder="(11) 94002-8922" />
           </View>
+          
+          <View>
+            <Text className="text-[20px] font-black font-inter-Black">Senha Atual</Text>
+            <Input className="w-[190px] text-gray-500 font-inter-regular" placeholder="********" />
+          </View>
         </View>
 
         <View className="gap-3">
@@ -38,42 +43,34 @@ export function ConfigUser({...props}: Models.IUserHook) {
             <Text className="text-[20px] font-black font-inter-Black">Escala Hoehn e Yahr</Text>
             <Input className="w-[190px] text-gray-500 font-inter-regular" />
           </View>
+
+          <View>
+            <Text className="text-[20px] font-black font-inter-Black">Nova Senha</Text>
+            <Input className="w-[190px] text-gray-500 font-inter-regular" placeholder="Digite sua nova senha" />
+            <Text className="text-[#1F4BFC] pt-2 text-[15px]">
+              Salvar nova senha
+            </Text>
+          </View>
         </View>
       </View>
 
-      <View className="flex items-center justify-center pt-8">
-        <Text className="text center font-inter-bold text-[25px]">Gêneros Musicais</Text>
-      </View>
-      <View className="mt-6">
-        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ paddingHorizontal: 8, gap: 8 }}>
-          {["Pop", "Rock", "Trap", "Jazz", "Clássica", "R&B"].map((item, index) => (
-            <View
-              key={index}
-              className={`w-[80px] h-[50px] rounded-lg flex items-center justify-center ${index === 2 ? "bg-[#1F4BFC]" : "bg-[#EDEDED]"}`}
-            >
-              <Text
-                style={{
-                  color: index === 2 ? "#FFF" : "#000",
-                  fontWeight: index === 2 ? "bold" : "normal",
-                }}
-              >
-                {item}
-              </Text>
-            </View>
-          ))}
-        </ScrollView>
-      </View>
 
-      <View className="border-b w-[380px] mx-auto p-7" style={{ opacity: 0.2 }}></View>
-
-      <View className="w-full flex-row justfy-around items-center mt-5 gap-2">
+      <View className="w-full flex-row justfy-around items-center mt-7 gap-2">
         <View className="gap-5 p-2.5">
           <Button text="Salvar dados" className="w-[190px] h-[50px] flex items-center justify-center text-center" onPress={() => changeState("view")} />
           <Button text="Excluir conta" className="w-[190px] h-[50px] flex items-center justify-center text-center bg-[#0D006F]" />
         </View>
-        <View className="w-[187px] h-[120px] bg-[#EDEDED] rounded-lg flex items-center justify-center">
-          <Text className="text-[60px] font-inter-regular text-center">90 bpm</Text>
-        </View>
+      <View className="gap-6 pl-2">
+        <Text className="text-[#1F4BFC] text-[15px] w-[120px]" style={{ opacity: 0.3 }}>
+          Problemas com a sua conta?
+        </Text>
+        <Text className="text-[#1F4BFC] text-[15px]" style={{ opacity: 0.3 }}>
+          Dúvidas? Fale Conosco
+        </Text>
+        <Text className="text-[#1F4BFC] text-[15px] w-[120px]" style={{ opacity: 0.3 }}>
+          Termos de Uso e Privacidade
+        </Text>
+      </View>
       </View>
     </Page>
   );
